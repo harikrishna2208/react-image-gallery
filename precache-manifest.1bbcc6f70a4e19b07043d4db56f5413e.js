e7cae2e635007d5b102551ed5053ebb911b35788
harikrishna2208 <harikrishna2278@gmail.com>
2020-09-05 14:16:37 +0530

    Updates

---
+++ precache-manifest.1bbcc6f70a4e19b07043d4db56f5413e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f78c82b83a14d2a72c8cd69b4a1b02",
+    "url": "/react-image-gallery/index.html"
+  },
+  {
+    "revision": "0cffd7a4bd306588e91f",
+    "url": "/react-image-gallery/static/css/main.6e787e22.chunk.css"
+  },
+  {
+    "revision": "e095af224f1b7b7b82eb",
+    "url": "/react-image-gallery/static/js/2.ece395d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-image-gallery/static/js/2.ece395d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cffd7a4bd306588e91f",
+    "url": "/react-image-gallery/static/js/main.2d8eb756.chunk.js"
+  },
+  {
+    "revision": "218113178022445cc026",
+    "url": "/react-image-gallery/static/js/runtime-main.ea331f80.js"
+  }
+]);
